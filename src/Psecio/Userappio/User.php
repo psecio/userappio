11af17a396ec6ebcee8b81d1aeb56ca93c0715c6
enygma <enygma@phpdeveloper.org>
2014-01-18 14:14:16 -0700

    adding user token handling to user login/logoff

--- src/Psecio/Userappio/User.php
+++ src/Psecio/Userappio/User.php
@@ -5,14 +5,32 @@ class User extends Base
 {
 	public function login($username, $password)
 	{
-		return $this->getService()->send(
+		$service = $this->getService();
+		$result = $service->send(
 			'user.login',
 			array('login' => $username, 'password' => $password)
 		);
+		// set the user token
+		$service->setUserToken($result->token);
+		$this->setService($service);
+
+		return $result;
 	}
 
 	public function save($data)
 	{
 		return $this->getService()->send('user.save', $data);
+	}
+
+	public function logout()
+	{
+		$service = $this->getService();
+		$result = $this->getService()->send('user.logout');
+
+		// clear out the user token
+		$service->clearUserToken();
+		$this->setService($service);
+
+		return $result;
 	}
 }
